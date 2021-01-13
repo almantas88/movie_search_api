1223388ae9a13a6f8cbd612680764464042fcc6c
unknown <almantas88@gmail.com>
2021-01-13 19:17:52 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-     hi
-    </div>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="container_image_input">
+        
+      </div>
+      <div className="button"></div>
+    </div>
+  );
+}
+
+export default Header;
