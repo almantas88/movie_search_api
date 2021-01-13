6a7680a7c64cdce2f53e9b0abec93ab42cdfd0fe
unknown <almantas88@gmail.com>
2021-01-13 22:03:12 +0200

    finished styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { ReactComponent as Movie } from "../images/movie.svg";
+import { ReactComponent as Search } from "../images/search.svg";
+
 import "./Header.css";
 
 function Header() {
   return (
     <div className="container">
-      <div className="container_image_input">
-        
+      <div className="container_input">
+        <Movie className="svg" />
+        <input className="input" placeholder="Enter movie name"/>
       </div>
-      <div className="button"></div>
+      <div className="button"><Search className="svg1" /></div>
     </div>
   );
 }
